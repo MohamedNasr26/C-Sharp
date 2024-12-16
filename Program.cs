b87d990655ebd78b9d9e80671848e5694f4e5448
moham <moham@ZAMALKAWY>
2024-12-16 22:39:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    int year ;
+
+
+            //    do {
+            //        Console.Write("Plz enter your year : -");
+            //        year = int.Parse(Console.ReadLine());
+
+            //    }while (year >2024);
+            //    Console.WriteLine(  year);
+
+            ////////////////////////////////    //////////////////////////////////
+            ///
+
+
+
+            Console.WriteLine("enter year: ");
+
+            int year;
+            bool flag = true;
+            while (flag)
+            {
+
+                year = int.Parse(Console.ReadLine());
+                if (year < 2024)
+                {
+
+                    flag = false;
+                    Console.WriteLine(year);
+                }
+                if (flag)
+                {
+                    Console.WriteLine("enter a vaild year: ");
+                }
+            }
+        }
+    }
+}
